2cf6999ab724aff613838db2411f366f4b14deb5
edson.adriano <edsoneurides0705@gmail.cpm>
2024-09-27 20:59:41 -0300

    remove gitignore

---
+++ docs/config.js
@@ -0,0 +1,4 @@
+const config = {
+    apiUrl: 'https://caoidydfeywjhhgyztrq.supabase.co/',
+    apiPublicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhb2lkeWRmZXl3amhoZ3l6dHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4NzIwNDgsImV4cCI6MjA0MjQ0ODA0OH0._T22SKD-XHRbdNyfjLlXPlOT-6BJJqFj0-6Zn-vO67U',
+};
